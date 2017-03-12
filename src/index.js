4b6d5081683be7bf9e83c473e98dd1ec1639eb86
Юра <Yura@orion.local>
2017-03-12 18:35:20 +0300

    try node

--- src/index.js
+++ src/index.js
@@ -2,21 +2,7 @@ import ReactDOM from 'react-dom';
 import { getRoutes } from './route';
 import './index.css';
 
-//import {App, Index, UsersIndex, User, Users, About} from './test'
-
 ReactDOM.render(
   getRoutes(),
   document.getElementById('root')
 );
-
-
-/*ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Index}/>
-      <Route path="/index" component={Index}/>
-      <Route path="/about" component={About}/>
-    </Route>
-  </Router>
-), document.getElementById('root'))
-*/
