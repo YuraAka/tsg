0579df3f47e77a8ea9ec1b0cd0cbb509baf0c670
Yura <yuraaka@gmail.com>
2017-04-16 23:54:50 +0300

    edit/preview mode

--- src/components/article_page.js
+++ src/components/article_page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Editor, EditorState, RichUtils, convertToRaw, convertFromRaw} from 'draft-js'
+import {stateToHTML} from 'draft-js-export-html'
 import ApiClient from '../service/api_client'
 
 class StyleButton extends React.Component {
@@ -30,14 +31,26 @@ class StylePanel extends React.Component {
   }
 }
 
-class EditableArticle extends React.Component {
-  constructor() {
-    super()
-    this.state = {editorState: EditorState.createEmpty()}
-    this.onChange = editorState => this.setState({editorState: editorState})
+export default class Article extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      readonly: true
+    }
+
+    /// todo apply good formatting from file:///Users/Yura/dev/react/draft-js/examples/draft-0-10-0/rich/rich.html
+    this.onChange = this._onChange.bind(this)
     this.onClick = this._onClick.bind(this)
     this.onShortcut = this._onShortCut.bind(this)
     this.onSave = this._onSave.bind(this)
+    this.onEdit = this._onEdit.bind(this)
+    this.onTab = this._onTab.bind(this) // todo not working
+    this.focus = () => this.refs.editor.focus()
+  }
+
+  _onChange(editorState) {
+    this.setState({editorState: editorState})
   }
 
   _onClick(action, inline) {
@@ -51,10 +64,14 @@ class EditableArticle extends React.Component {
   _onShortCut(command) {
     const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
     if (newState) {
-      this.onChange(newState);
-      return 'handled';
+      this.onChange(newState)
+      return true
     }
-    return 'not-handled';
+    return false
+  }
+
+  _onTab(e) {
+    this.onChange(RichUtils.onTab(e, this.state.editorState, 2))
   }
 
   _onSave() {
@@ -63,7 +80,7 @@ class EditableArticle extends React.Component {
       {
         onSuccess: (ans) => {
           this.setState({
-            saved: true
+            readonly: true
           })
         },
         onFail: () => {
@@ -73,10 +90,16 @@ class EditableArticle extends React.Component {
         }
       },
       {
-        id: this.props.id,
+        id: this.props.params.id,
         data: content
       }
     )
+  }
+
+  _onEdit() {
+    this.setState({
+      readonly: false
+    })
   }
 
   componentDidMount() {
@@ -91,31 +114,27 @@ class EditableArticle extends React.Component {
           }
         }
       },
-      this.props.id
+      this.props.params.id
     )
   }
 
-  render() {
-    return (
-      <div style={{marginBottom: 10}}> 
-        <StylePanel onClick={this.onClick}/>
-        <Editor 
-          editorState={this.state.editorState} 
-          onChange={this.onChange}
-          handleKeyCommand={this.onShortcut}
-        />
-        <button onClick={this.onSave}>Сохранить</button>
-      </div>
-    )
-  }
-}
-
-export default class Article extends React.Component {
   render() {
     return (
       <div>
-        <div>Article id: {this.props.params.id}</div>
-        <EditableArticle id={this.props.params.id}/>
+        {!this.state.readonly && <StylePanel onClick={this.onClick}/>}
+        <div onClick={this.focus}>
+          <Editor 
+            editorState={this.state.editorState} 
+            onChange={this.onChange}
+            handleKeyCommand={this.onShortcut}
+            readOnly={this.state.readonly}
+            spellCheck={true}
+            onTab={this.onTab}
+            ref='editor'
+          />
+        </div>
+        {!this.state.readonly && <button onClick={this.onSave}>Сохранить</button>}
+        {this.state.readonly && <button onClick={this.onEdit}>Изменить</button>}
       </div>
     )
   }
